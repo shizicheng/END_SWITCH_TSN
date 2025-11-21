53e17261cead566b693bc3c9c6ca4f7e4c1c78fa
shann121 <13917476934@163.com>
2025-11-21 13:31:16 +0800

    Qav信用值减小支持负值，locredit>=0时可以允许发送

--- switch_core/txmac/scheduling_pipeline/tsn_qav_mng.v
+++ switch_core/txmac/scheduling_pipeline/tsn_qav_mng.v
@@ -40,6 +40,7 @@ module  tsn_qav_mng #(
     input               wire   [15:0]                           i_lothreshold_q7        ,
     input               wire                                    i_config_vld            ,
     input               wire                                    i_qav_en                ,
+	input				wire									i_send_flag				,
     /*------------------------------ 调度信息输入 ------------------------------*/
     input               wire   [PORT_FIFO_PRI_NUM-1:0]          i_fifoc_empty           , // 实时检测该端口对应 CROSSBAR 交换平面优先级 FIFO 信息
     input               wire   [PORT_FIFO_PRI_NUM-1:0]          i_scheduing_rst         , // 该端口调度流水线产生的调度结果
@@ -55,12 +56,12 @@ module  tsn_qav_mng #(
 wire        [7:0]                   w_idleSlope[0:PORT_FIFO_PRI_NUM-1];
 wire        [7:0]                   w_sendslope[0:PORT_FIFO_PRI_NUM-1];
 wire        [15:0]                  w_hithreshold[0:PORT_FIFO_PRI_NUM-1];
-wire        [15:0]                  w_lothreshold[0:PORT_FIFO_PRI_NUM-1];
+wire        [16:0]                  w_lothreshold[0:PORT_FIFO_PRI_NUM-1];
 
 reg         [7:0]                   ri_admin_idleSlope[0:PORT_FIFO_PRI_NUM-1]      ;
 reg         [7:0]                   ri_admin_sendslope[0:PORT_FIFO_PRI_NUM-1]      ;
 reg         [15:0]                  ri_admin_hithreshold[0:PORT_FIFO_PRI_NUM-1]    ;
-reg         [15:0]                  ri_admin_lothreshold[0:PORT_FIFO_PRI_NUM-1]    ;
+reg         [16:0]                  ri_admin_lothreshold[0:PORT_FIFO_PRI_NUM-1]    ;
 
 reg                                 r_config_proc           ;
 reg                                 r0_config_proc          ;
@@ -68,9 +69,9 @@ reg                                 r0_config_proc          ;
 reg         [7:0]                   r_exe_idleSlope[0:PORT_FIFO_PRI_NUM-1]         ;
 reg         [7:0]                   r_exe_sendslope[0:PORT_FIFO_PRI_NUM-1]         ;
 reg         [15:0]                  r_exe_hithreshold[0:PORT_FIFO_PRI_NUM-1]       ;
-reg         [15:0]                  r_exe_lothreshold[0:PORT_FIFO_PRI_NUM-1]       ;
+reg         [16:0]                  r_exe_lothreshold[0:PORT_FIFO_PRI_NUM-1]       ;
 
-reg         [15:0]                  queue_av_data   [7:0]   ; // 0 - 32768
+reg         [16:0]                  queue_av_data   [7:0]   ; // 0 - 32768 {1bit符号位+15bit数据位}
 reg         [PORT_FIFO_PRI_NUM-1:0] send_pri_flag           ;
 reg                                 send_flag               ; // 是否有帧处于发送状态
 reg         [PORT_FIFO_PRI_NUM-1:0] ro_queue                ;
@@ -127,35 +128,35 @@ end
 assign w_idleSlope[0]   = i_idleSlope_q0;
 assign w_sendslope[0]   = i_sendslope_q0;
 assign w_hithreshold[0] = i_hithreshold_q0;
-assign w_lothreshold[0] = i_lothreshold_q0;
+assign w_lothreshold[0] = ~i_lothreshold_q0 + 1'b1;
 assign w_idleSlope[1]   = i_idleSlope_q1;
 assign w_sendslope[1]   = i_sendslope_q1;
 assign w_hithreshold[1] = i_hithreshold_q1;
-assign w_lothreshold[1] = i_lothreshold_q1;
+assign w_lothreshold[1] = ~i_lothreshold_q1 + 1'b1;
 assign w_idleSlope[2]   = i_idleSlope_q2;
 assign w_sendslope[2]   = i_sendslope_q2;
 assign w_hithreshold[2] = i_hithreshold_q2;
-assign w_lothreshold[2] = i_lothreshold_q2;
+assign w_lothreshold[2] = ~i_lothreshold_q2 + 1'b1;
 assign w_idleSlope[3]   = i_idleSlope_q3;
 assign w_sendslope[3]   = i_sendslope_q3;
 assign w_hithreshold[3] = i_hithreshold_q3;
-assign w_lothreshold[3] = i_lothreshold_q3;
+assign w_lothreshold[3] = ~i_lothreshold_q3 + 1'b1;
 assign w_idleSlope[4]   = i_idleSlope_q4;
 assign w_sendslope[4]   = i_sendslope_q4;
 assign w_hithreshold[4] = i_hithreshold_q4;
-assign w_lothreshold[4] = i_lothreshold_q4;
+assign w_lothreshold[4] = ~i_lothreshold_q4 + 1'b1;
 assign w_idleSlope[5] = i_idleSlope_q5;
 assign w_sendslope[5] = i_sendslope_q5;
 assign w_hithreshold[5] = i_hithreshold_q5;
-assign w_lothreshold[5] = i_lothreshold_q5;
+assign w_lothreshold[5] = ~i_lothreshold_q5 + 1'b1;
 assign w_idleSlope[6] = i_idleSlope_q6;
 assign w_sendslope[6] = i_sendslope_q6;
 assign w_hithreshold[6] = i_hithreshold_q6;
-assign w_lothreshold[6] = i_lothreshold_q6;
+assign w_lothreshold[6] = ~i_lothreshold_q6 + 1'b1;
 assign w_idleSlope[7] = i_idleSlope_q7;
 assign w_sendslope[7] = i_sendslope_q7;
 assign w_hithreshold[7] = i_hithreshold_q7;
-assign w_lothreshold[7] = i_lothreshold_q7;
+assign w_lothreshold[7] = ~i_lothreshold_q7 + 1'b1;
 
 genvar i;
 generate
@@ -165,7 +166,7 @@ generate
                 ri_admin_idleSlope[i]    <= 8'd0;
                 ri_admin_sendslope[i]    <= 8'd0;
                 ri_admin_hithreshold[i]  <= 16'd0;
-                ri_admin_lothreshold[i]  <= 16'd0;
+                ri_admin_lothreshold[i]  <= 17'd0;
             end else begin
                 ri_admin_idleSlope[i]    <= (i_config_vld == 1'b1) ? w_idleSlope[i] : ri_admin_idleSlope[i];
                 ri_admin_sendslope[i]    <= (i_config_vld == 1'b1) ? w_sendslope[i] : ri_admin_sendslope[i];
@@ -178,7 +179,7 @@ generate
                 r_exe_idleSlope[i]       <= 8'd0;
                 r_exe_sendslope[i]       <= 8'd0;
                 r_exe_hithreshold[i]     <= 16'd0;
-                r_exe_lothreshold[i]     <= 16'd0;
+                r_exe_lothreshold[i]     <= 17'd0;
             end else begin
                 r_exe_idleSlope[i]       <= (i_mac_tx_axis_valid == 1'b0) ? ri_admin_idleSlope[i] : r_exe_idleSlope[i];
                 r_exe_sendslope[i]       <= (i_mac_tx_axis_valid == 1'b0) ? ri_admin_sendslope[i] : r_exe_sendslope[i];
@@ -226,24 +227,23 @@ generate
                 send_pri_flag[i] <= 1'b0;
             end else begin
                 send_pri_flag[i] <= (i_mac_tx_axis_last == 1'b1 && i_mac_tx_axis_valid == 1'b1 && i_scheduing_rst[i] == 1'b1) ? 1'b0 : 
-                                    (i_scheduing_rst_vld == 1'b1 && i_scheduing_rst[i] == 1'b1) ? 1'b1 : send_pri_flag[i];
+                                    (i_send_flag == 1'b1 && i_scheduing_rst[i] == 1'b1) ? 1'b1 : send_pri_flag[i];
             end
         end
         always @(posedge i_clk or posedge i_rst) begin
             if (i_rst) begin
-                queue_av_data[i] <= 16'd0;
+                queue_av_data[i] <= 17'd0;
             end else begin
                 queue_av_data[i] <= (r0_config_proc == 1'b1 && r_config_proc == 1'b0) ? r_exe_lothreshold[i] : 
-                (send_pri_flag[i] == 1'b1 && send_flag == 1'b1 && queue_av_data[i] != 16'h0000) ? (queue_av_data[i] - r_exe_sendslope[i]) : 
-                (send_pri_flag[i] == 1'b1 && send_flag == 1'b1 && queue_av_data[i] == 16'h0000) ? queue_av_data[i] :
-                (send_pri_flag[i] == 1'b0 && queue_av_data[i] != r_exe_hithreshold[i]) ? (queue_av_data[i] + r_exe_idleSlope[i]) : queue_av_data[i];
+                (send_pri_flag[i] == 1'b1) ? (queue_av_data[i] - {1'b0,r_exe_sendslope[i]}) : 
+                (send_pri_flag[i] == 1'b0 && queue_av_data[i] != {1'b0,r_exe_hithreshold[i]}) ? (queue_av_data[i] + {1'b0,r_exe_idleSlope[i]}) : queue_av_data[i];
             end
         end
         always @(posedge i_clk or posedge i_rst) begin
             if (i_rst) begin
-                r_av_rst[i] <= {(PORT_FIFO_PRI_NUM){1'b0}};
+                r_av_rst <= {(PORT_FIFO_PRI_NUM){1'b0}};
             end else begin
-                r_av_rst[i] <= ((queue_av_data[i] >= r_exe_lothreshold[i]) && ri_fifoc_empty[i] == 1'b0) ? 1'b1 : 1'b0;  
+                r_av_rst[i] <= ((queue_av_data[i][16] == 1'b0) && ri_fifoc_empty[i] == 1'b0) ? 1'b1 : 1'b0;  
             end
         end
     end
